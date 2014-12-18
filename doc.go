535af1dd260347f47c41cdb35ef966d5a1e49a36
Rafael Dantas Justo <rafael@registro.br>
2014-12-18 10:12:39 -0200

    Better doc format

--- doc.go
+++ doc.go
@@ -9,36 +9,30 @@
 // We took some decisions when creating this library taking into account that less is more. The
 // decisions are all listed bellow.
 //
-// * Always retrieving the last index
+// Always retrieving the last index: For the use case that we thought, there's no reason (for now)
+// to retrieve an intermediate state of a field. We are always looking for the current value in
+// etcd. But we store the index of all attributes retrieved from etcd so that the user wants to know
+// it (in the library we use "version" instead of "index" because it appears to have a better
+// context).
 //
-//   For the use case that we thought, there's no reason (for now) to retrieve an intermediate state
-//   of a field. We are always looking for the current value in etcd. But we store the index of all
-//   attributes retrieved from etcd so that the user wants to know it (in the library we use
-//   "version" instead of "index" because it appears to have a better context).
+// Setting unlimited TTL: The type of data that we store in etcd (configuration values) don't need a
+// TTL. Or at least we did not imagine any case when it does need a TTL.
 //
-// * Setting unlimited TTL
+// Ignoring errors occurred in watch: When something goes wrong while retrieving or parsing the data
+// from etcd, we prefer to silent drop the update instead of setting a strange value to the
+// configuration field. Another problem is to create a good API to notify about errors occurred in
+// watch, the first idea is to use a channel for errors, but it doesn't appears to be a elegant
+// approach, and more than that, what the user can do with this error? Well, we are still thinking
+// about it.
 //
-//   The type of data that we store in etcd (configuration values) don't need a TTL.
+// Ignoring "directory already exist" errors: If the directory already exists, great! We go on and
+// create the structure under this directory. There's no reason to stop everything because of this
+// error.
 //
-// * Ignoring errors occurred in watch
-//
-//   When something goes wrong while retrieving or parsing the data from etcd, we prefer to silent
-//   drop the update instead of setting a strange value to the configuration field. Another problem
-//   is to create a good API to notify about errors occurred in watch, the first idea is to use a
-//   channel for errors, but it doesn't appears to be a elegant approach, and more than that, what
-//   the user can do with this error? Well, we are still thinking about it.
-//
-// * Ignoring "directory already exist" errors
-//
-//   If the directory already exists, great! We go on and create the structure under this directory.
-//   There's no reason to stop everything because of this error.
-//
-// * Not allowing URI in structure's field tag
-//
-//   This was a change made on 2014-12-17. I thought that leaving the decision to the user to create
-//   an URI in a structure's field tag could cause strange behaviors when structuring the
-//   configuration in etcd. So all the slashes in the field's tag will be replaced by hyphens,
-//   except if the slash is the first or last character
+// Not allowing URI in structure's field tag: This was a change made on 2014-12-17. I thought that
+// leaving the decision to the user to create an URI in a structure's field tag could cause strange
+// behaviors when structuring the configuration in etcd. So all the slashes in the field's tag will
+// be replaced by hyphens, except if the slash is the first or last character
 //
 // Improve
 //
